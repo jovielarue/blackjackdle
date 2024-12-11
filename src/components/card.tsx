6132b2222a3813241763463ff40affeef020015a
jovielarue <44442031+jovielarue@users.noreply.github.com>
2024-12-11 16:24:22 -0700

    deleted oops on main

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,25 +1,10 @@
 import React from 'react';
 import {StyleSheet, View, Text} from 'react-native';
-import CardImage from './card-image';
 
 export default function Card(props: {title: string}) {
-  const value = props.title.split(':')[0];
-  const suite = props.title.split(':')[1];
-
-  let cardFileName;
-  let cardImg;
-  if (value.length === 1 && suite.length === 1) {
-    cardFileName = '../assets/cards/' + value + '-' + suite + '.png';
-    console.log(cardFileName);
-  }
-
   return (
     <View style={styles.cardView}>
-      {cardFileName === undefined || cardImg === undefined ? (
-        <Text style={styles.cardText}>{props.title}</Text>
-      ) : (
-        <CardImage source={cardFileName} cardStyle={styles.cardImg} />
-      )}
+      <Text style={styles.cardText}>{props.title}</Text>
     </View>
   );
 }
